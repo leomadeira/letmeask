cbba4cee3c1cb929e4bdf60036003d5a46853e99
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-24 21:19:35 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,26 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import { AuthContext } from '../App';
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-import '../style/auth.scss';
 import { Button } from '../components/Button';
 
-import { useNavigate } from 'react-router-dom'
-import { auth, firebase } from '../services/firebase'
+import '../style/auth.scss';
 
 export function Home(){
 
     const navigate = useNavigate(); 
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
-    function nagigateToNewRoom(){
-        //const provider = new firebase.auth.GoogleAuthProvider();
-
-        //auth.signInWithPopup(provider).then(result => {
-        //    console.log(result);
-        //})
-        navigate.push('/rooms')
+    async function handleCreateRoom(){
+        if(!user){ //Se o usuario nao estiver autenticado
+           await signInWithGoogle()
+        }
+        navigate('/rooms') //se estiver ele entra nessa pagina
     }
 
     return(
@@ -31,7 +33,7 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="letmeask" />
-                    <button onClick={nagigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
